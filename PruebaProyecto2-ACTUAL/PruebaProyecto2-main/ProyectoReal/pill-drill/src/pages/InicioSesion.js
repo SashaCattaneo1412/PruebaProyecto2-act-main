fb212aa5064017f3e70b08ae196ad34388963ae3
SashaCattaneo1412 <sashaacattaneo@gmail.com>
2023-08-07 14:52:10 -0300

    Initial commit

---
+++ PruebaProyecto2-ACTUAL/PruebaProyecto2-main/ProyectoReal/pill-drill/src/pages/InicioSesion.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Layout from '../components/Layout'
+ import Link from 'next/link';
+ import PhotoCarousel from '../components/PhotoCarousel.js';
+ import style from '../styles/Registrarse.module.css'
+ const InicioSesion = () => {
+   return (
+    <div>
+      <h1 className={style.title}>BIENVENIDO A PILL-DRILL</h1>
+     <PhotoCarousel />
+       
+     <main className={style.flexbox}>
+        <form>
+
+          <div className={style.contenedorflotante}>
+            <input type="INPUT" className="form-control" id="floatingInput" placeholder="Mail" />
+          </div>
+
+
+            <div className={style.contenedorflotante}>
+            <input type="password" className="form-control" id="floatingPassword" placeholder=" Contraseña" />
+            </div>
+
+          <div>
+            <label className={style.seguimiento}>
+            
+            </label>
+          </div>
+
+        
+
+          <div>
+            <label className={style.cuenta}>
+            ¿Olvidaste tu contraseña? 
+            </label>
+            <Link href="/InicioSesion"> Recuperala</Link>
+          </div>
+
+          <div>
+            <label className={style.cuenta}>
+            ¿No tenes cuenta?
+            </label>
+            <Link href="/Registrarse">  Crea cuenta nueva</Link>
+          </div>
+           
+          <button className="w-100 btn btn-lg btn-primary" type="submit" href="/compartieminto">IR</button>
+
+
+
+        </form>
+      </main>
+
+
+    </div>
+   )
+ }
+ 
+ export default InicioSesion
---
+++ PruebaProyecto2-ACTUAL/PruebaProyecto2-main/ProyectoReal/pill-drill/src/pages/compartimiento.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../components/Layout'
+ import Link from 'next/link';
+
+
+
+const Compartimeinto = () =>
+
+{
+    return (
+        
+     
+    
+     <h1>SELECCIONE EL COMPARTIMEINTO</h1>
+     
+        
+      );
+}
+
+export default Compartimeinto;
